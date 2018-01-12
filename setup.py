e573d50160a032b938efda697b0d50f904adca69
Randy <rsm5139@psu.edu>
2018-01-12 15:10:23 -0500

    Updated URL

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Converts PaperPile JSON exports into YAML suitable for Jekyll websites.',
-    url='https://github.com/rsm5139/paperpileConvert',
+    url='https://github.com/scrim-network/paperpileConvert',
     install_requires=['latexcodec'],
     author='Randy Miller',
     author_email='rsm5139@psu.edu'
